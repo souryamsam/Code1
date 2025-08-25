f3874c2a3aa3500ebd3100a132bbcb542a148b6d
Souryam Sam <souryamsam0@gmail.com>
2025-08-25 14:36:21 +0530

    New file add

---
+++ task1.py
@@ -0,0 +1,8 @@
+#Task 1: Perform Basic Mathematical Operations
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+
+print("addition =",a+b)
+print("subtraction =",a-b)
+print("multiplication =",a*b)
+print("division =",a/b)
---
+++ task2.py
@@ -0,0 +1,4 @@
+#Task 2: Create a Personalized Greeting
+a=input("Enter the first name: ")
+b=input("Enter the last name: ")
+print("Hello, "+a+" "+b+"! Welcome to the Python program.")
